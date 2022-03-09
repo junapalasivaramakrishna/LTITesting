027b871a204596ece5c5f4a0c81ebe7ad6e20ffa
junapalasivaramakrishna <101242802+junapalasivaramakrishna@users.noreply.github.com>
2022-03-09 14:27:36 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,10 @@
+class addition
+{
+    public static void main(String[] args)
+    {
+     int a=30;
+     int b=60;
+     int res=(a+b);
+     System.out.println(res);
+    }
+}
---
+++ area of rectangle.java
@@ -0,0 +1,9 @@
+class area of sqare
+{
+public static void main(String[] args)
+{
+int length=24;
+int area=length*length;
+System.out.println(area);
+}
+}
